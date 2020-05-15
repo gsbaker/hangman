0883650b2e9af4b6c5e2ed13576df16077173c36
George Baker <gsbaker2000@icloud.com>
2020-05-15 17:23:36 +0100

    Initial commit

---
+++ Hangman/GuessViewController.swift
@@ -0,0 +1,30 @@
+//
+//  GuessViewController.swift
+//  Hangman
+//
+//  Created by George Baker on 15/05/2020.
+//  Copyright © 2020 gsbaker. All rights reserved.
+//
+
+import UIKit
+
+class GuessViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
